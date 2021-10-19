c330051af491680a8ce3a33f718ab35930a870dd
hrithikh <hrithik.h@optimizeitsystems.com>
2021-10-19 21:04:10 +0530

    first commit

---
+++ src/main/java/net/hrithik/springboot/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package net.hrithik.springboot.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import net.hrithik.springboot.model.Employee;
+
+public interface EmployeeService {
+	List<Employee> getAllEmployees();
+
+	void saveEmployee(Employee employee);
+
+	Employee getEmployeeById(long id);
+
+	void deleteEmployeeById(long id);
+
+	Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
---
+++ src/main/resources/templates/js/ext.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+//    $('input[type="text"]').focus(function(){
+//        this_val=$(this);
+//        minlength=this_sel.attr('minlength');
+//    });
+
+
+
+    
+//     $("form").submit(function(){
+//         $flag=true;
+//         $(this).find(".required").each(function(){
+//             if($(this).val()===""){
+//                 $flag=false;
+//                 $(this).addClass("blank").siblings(".red").children("small").html($(this).attr("firstName")+"filed is required");
+                
+//             }
+//             else
+//             $(this).removeClass("blank").siblings(".red").children("small").html("");
+//         });
+//         return $flag;
+    
+//     })  
+// });
